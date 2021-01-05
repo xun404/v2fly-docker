1ca5e7edaf8e3b440f76061dae38bbcb6279d908
I'm xun <mx@rmbz.net>
2021-01-05 15:53:45 +0800

    update v2log.sh

--- v2log.sh
+++ v2log.sh
@@ -35,36 +35,37 @@ fi
 [ -z "${ARCH}" ] && echo "Error: Not supported OS Architecture" && exit 1
 
 # Download files
-V2RAY_FILE="v2ray-linux-${ARCH}.zip"
-DGST_FILE="v2ray-linux-${ARCH}.zip.dgst"
-echo "Downloading binary file: ${V2RAY_FILE}"
+V2LOG_FILE="v2log-linux-${ARCH}.zip"
+DGST_FILE="v2log-linux-${ARCH}.zip.dgst"
+echo "Downloading binary file: ${V2LOG_FILE}"
 echo "Downloading binary file: ${DGST_FILE}"
 
 TAG=$(wget -qO- https://raw.githubusercontent.com/v2fly/docker/master/ReleaseTag | head -n1)
-wget -O ${PWD}/v2ray.zip https://github.com/v2fly/v2ray-core/releases/download/${TAG}/${V2RAY_FILE} > /dev/null 2>&1
-wget -O ${PWD}/v2ray.zip.dgst https://github.com/v2fly/v2ray-core/releases/download/${TAG}/${DGST_FILE} > /dev/null 2>&1
+wget -O ${PWD}/v2log.zip https://github.com/v2fly/v2ray-core/releases/download/${TAG}/${V2LOG_FILE} > /dev/null 2>&1
+wget -O ${PWD}/v2log.zip.dgst https://github.com/v2fly/v2ray-core/releases/download/${TAG}/${DGST_FILE} > /dev/null 2>&1
 
 if [ $? -ne 0 ]; then
-    echo "Error: Failed to download binary file: ${V2RAY_FILE} ${DGST_FILE}" && exit 1
+    echo "Error: Failed to download binary file: ${V2LOG_FILE} ${DGST_FILE}" && exit 1
 fi
-echo "Download binary file: ${V2RAY_FILE} ${DGST_FILE} completed"
+echo "Download binary file: ${V2LOG_FILE} ${DGST_FILE} completed"
 
 # Check SHA512
-LOCAL=$(openssl dgst -sha512 v2ray.zip | sed 's/([^)]*)//g')
-STR=$(cat v2ray.zip.dgst | grep 'SHA512' | head -n1)
+LOCAL=$(openssl dgst -sha512 v2log.zip | sed 's/([^)]*)//g')
+STR=$(cat v2log.zip.dgst | grep 'SHA512' | head -n1)
 
 if [ "${LOCAL}" = "${STR}" ]; then
-    echo " Check passed" && rm -fv v2ray.zip.dgst
+    echo " Check passed" && rm -fv v2log.zip.dgst
 else
     echo " Check have not passed yet " && exit 1
 fi
 
 # Prepare
 echo "Prepare to use"
-unzip v2ray.zip && chmod +x v2ray v2ctl
-mv v2ray v2ctl /usr/bin/
-mv geosite.dat geoip.dat /usr/local/share/v2ray/
-mv config.json /etc/v2ray/config.json
+unzip v2log.zip && chmod +x v2ray v2ctl
+mv v2ray v2log
+mv v2log v2ctl /usr/bin/
+mv geosite.dat geoip.dat /usr/local/share/v2log/
+mv config.json /etc/v2log/config.json
 
 # Clean
 rm -rf ${PWD}/*
